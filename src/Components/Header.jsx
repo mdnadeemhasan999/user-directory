8ea39d71f90f2ba18037efb7364143b63000b195
mdnadeem-codingexpo <101696389+mdnadeem-codingexpo@users.noreply.github.com>
2023-12-23 01:58:21 +0530

    time zone WIP

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useData } from '../DataContext';
 
 const Header = () => {
   const { countries } = useData();
-  const [selectedCountry, setSelectedCountry] = useState('test');
+  const [selectedCountry, setSelectedCountry] = useState('America/Argentina/Tucuman');
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [isPaused, setIsPaused] = useState(false);
 
   const handleCountryChange = (e) => {
     setSelectedCountry(e.target.value);
-    console.log(e.target.value);
+  };
+
+  const fetchData = async () => {
+    if (selectedCountry) {
+      try {
+        const response = await fetch(`http://worldtimeapi.org/api/timezone/${selectedCountry}`);
+        const data = await response.json();
+        const utcTimeInMillis = new Date(data.utc_datetime).getUTCDate();
+        const offsetInMillis = -3 * 60 * 60 * 1000; // -3 hours in milliseconds
+        const adjustedTimeInMillis = utcTimeInMillis + offsetInMillis;
+        console.log(adjustedTimeInMillis);
+        setCurrentTime(new Date(adjustedTimeInMillis));
+      } catch (error) {
+        console.error('Error fetching time data:', error);
+      }
+    } else {
+      setCurrentTime(new Date());
+    }
+  };
+
+  const handlePauseClick = () => {
+    setIsPaused(!isPaused);
   };
 
   const navigate = useNavigate();
 
   const handleBackClick = () => {
     navigate(-1);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedCountry, isPaused]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!isPaused) {
+        fetchData();
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [isPaused]);
+
+  // Assuming you have a function to format the date as a string
+  const formatTime = (time) => {
+    return time.toLocaleTimeString();
   };
 
   return (
@@ -26,15 +68,18 @@ const Header = () => {
       </div>
       <div className='pause'>
         <label htmlFor="country">Country</label>
-        <select id="country"  value={selectedCountry} onChange={handleCountryChange}>
-            {countries.map((country) => (
-                <option key={country} value={country}>
-                {country}
-                </option>
-            ))}
+        <select id="country" value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">Select...</option>
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
         </select>
-        <p>clock</p>
-        <button className='pause-btn'>Pause/Start</button>
+        <p>Current Time: {formatTime(currentTime)}</p>
+        <button className='pause-btn' onClick={handlePauseClick}>
+          {isPaused ? 'Start' : 'Pause'}
+        </button>
       </div>
     </div>
   );
